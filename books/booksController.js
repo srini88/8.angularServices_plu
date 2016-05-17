a66f20ed7cb557467ee1c6adeb09ba480fd8d28e
srini88 <pvsrinivas.ece@gmail.com>
2016-05-17 14:17:10 -0500

    cleanup

--- books/booksController.js
+++ books/booksController.js
@@ -5,6 +5,7 @@ angular.module('app').
 controller('BooksController', ['books', 'dataService', 'logger', 'badgeService','$q' ,'$cookies','$cookieStore', '$log',BooksController]);
 
 //update booksController and store that shit in the view..
+//books is the provider that gives name and desc - it is in js.
 function BooksController(books, dataService, logger, badgeService, $q, $cookies, $cookieStore, $log){
 
 	var vm = this;
@@ -27,12 +28,6 @@ function BooksController(books, dataService, logger, badgeService, $q, $cookies,
 	logger.output("BooksController has been created.."); 
 	vm.favoriteBook = $cookies.favoriteBook; 
 	vm.lastEdited = $cookieStore.get('lastEdited');
-
-	$log.log("logging with log");
-	$log.info("logging with info");
-	$log.warn("logging with warn");
-	$log.error("logging with error");
-	$log.debug("logging with debug");  //all stuff shows in different colors..
 }
 
 })();
